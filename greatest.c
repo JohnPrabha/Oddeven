5df4d0b47d940f569758c3f16ad8a70200500963
JohnPrabha <40799005+JohnPrabha@users.noreply.github.com>
2018-07-13 09:50:37 +0530

    Add files via upload

---
+++ greatest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    double n1, n2, n3;
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    if (n1>=n2)
+    {
+        if(n1>=n3)
+            printf("%.2lf",n1);
+        else
+            printf("%.2lf",n3);
+    }
+    else
+    {
+        if(n2>=n3)
+            printf("%.2lf",n2);
+        else
+            printf("%.2lf",n3);
+    }
+    return 0;
+}
